8f66205a8106e49a0da20d5e763698c9d1576753
GuruDev89 <itbeaty0728@gmail.com>
2020-03-09 05:47:15 +0200

    initial commit

---
+++ src/apis/index.js
@@ -0,0 +1,5 @@
+export const BASE_URL='https://backend-gurudev.herokuapp.com';
+export const SEND_EMAIL='/emails/send';
+export const TRACK_IP='/track';
+export const FETCH_VISITOR='/track/fetch';
+export const GET_RESUME='/resume/';
