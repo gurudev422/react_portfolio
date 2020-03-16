ed1096d090b280f5e8a6d46271324225cc6f5e0e
GuruDev89 <itbeaty0728@gmail.com>
2020-03-16 18:03:27 +0200

    updates

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -6,7 +6,7 @@ export default class About extends Component {
       <section id="about">
          <div className="row">
             <div className="three columns">
-               <img className="profile-pic"  src="images/profile.png" alt="" />
+               <img className="profile-pic"  src="images/0.png" alt="" />
             </div>
 
             <div className="nine columns main-col">
